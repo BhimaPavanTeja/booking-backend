3e383f0d2276484839a61fc758805307edc40502
Bhima Pavan Teja <124360829+BhimaPavanTeja@users.noreply.github.com>
2026-01-22 15:31:34 +0530

    Add route to get all bookings

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const adminController = require("../controllers/adminController")
 
 router.post("/bookings/:bookingId/override", adminController.overrideBookingStatus)
+router.get("/bookings", adminController.getAllBookings)
 
 module.exports = router
